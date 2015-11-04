f77dd2ddcf6f90f135ab50f8c9d7a06397b7b594
ironchrissi <mail@ironchrissi.de>
2015-11-04 23:00:26 +0100

    Add Output Classes for HTML und XML Output

--- libs/Output/Html.php
+++ libs/Output/Html.php
@@ -1,1 +1,20 @@
 <?php
+/**
+ * Html Output Class
+ *
+ * @category  Output
+ * @package   PhpDatabaseAnalyzer
+ * @author    ironchrissi <mail@ironchrissi.de>
+ * @copyright Copyright (c) 2015
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link      https://github.com/ironchrissi/PhpDatabaseAnalyzer
+ * @version   0.1
+ **/
+
+namespace Output;
+
+class Html
+{
+    public function __construct()
+    {}
+}

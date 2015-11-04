d27f5431af8ce87c71e81bc716b3466671ebe20f
ironchrissi <mail@ironchrissi.de>
2015-11-04 22:49:55 +0100

    Add Class Comment

--- libs/Databases/MySQL/Connection.php
+++ libs/Databases/MySQL/Connection.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Mysqli Connection Class
+ *
+ * @category  Database
+ * @package   PhpDatabaseAnalyzer
+ * @author    ironchrissi <mail@ironchrissi.de>
+ * @copyright Copyright (c) 2015
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link      https://github.com/ironchrissi/PhpDatabaseAnalyzer
+ * @version   0.1
+ **/
+
 namespace Databases\Mysql;
 
 class Connection

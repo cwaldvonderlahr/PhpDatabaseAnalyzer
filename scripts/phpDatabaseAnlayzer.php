9f566f49fdaa25ebb5bedb1af4832167e85cebb1
Frederik Glücks <frederik@gluecks-online.de>
2015-11-11 23:52:06 +0100

    Command line script for PhpDatabaseAnalyzer start added.

--- scripts/phpDatabaseAnlayzer.php
+++ scripts/phpDatabaseAnlayzer.php
@@ -1,1 +1,14 @@
 <?php
+/**
+ * Autoloader
+ */
+
+$loader = require dirname(__FILE__).'/../vendor/autoload.php';
+
+/**
+ * run database analyzer
+ */
+
+$PhpDatabaseAnalyzer = new \PhpDatabaseAnalyzer\PhpDatabaseAnalyzer($argv[1]);
+
+$PhpDatabaseAnalyzer->start();

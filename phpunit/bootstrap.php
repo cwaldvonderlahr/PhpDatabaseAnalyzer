187f59b69ea1c57dd180595bd59936f9860e441a
Frederik Glücks <frederik@gluecks-online.de>
2015-11-11 23:05:43 +0100

    Basis configuration for PHPUnit-Tests

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -1,1 +1,13 @@
 <?php
+$config = array(
+    'Zend\Loader\StandardAutoloader' => array(
+        'namespaces' => array(
+            'PhpDatabaseAnalyzer' => dirname(__FILE__) . "/../libs/"
+        )
+    )
+);
+
+require_once 'Zend/Loader/AutoloaderFactory.php';
+require_once 'Zend/Loader/StandardAutoloader.php';
+require_once 'Zend/Loader/ClassMapAutoloader.php';
+Zend\Loader\AutoloaderFactory::factory($config);

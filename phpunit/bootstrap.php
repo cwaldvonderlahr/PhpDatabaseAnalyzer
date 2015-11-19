85483bda937981efaadda78808b621b03faca025
Frederik Glücks <frederik@gluecks-online.de>
2015-11-19 20:05:08 +0100

    Changed autoloader in PHPUnit bootstrap.php from ZendFramework to Composer autoloader

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -1,13 +1,2 @@
 <?php
-$config = array(
-    'Zend\Loader\StandardAutoloader' => array(
-        'namespaces' => array(
-            'PhpDatabaseAnalyzer' => dirname(__FILE__) . "/../libs/"
-        )
-    )
-);
-
-require_once 'Zend/Loader/AutoloaderFactory.php';
-require_once 'Zend/Loader/StandardAutoloader.php';
-require_once 'Zend/Loader/ClassMapAutoloader.php';
-Zend\Loader\AutoloaderFactory::factory($config);
+$loader = require dirname(__FILE__).'/../vendor/autoload.php';

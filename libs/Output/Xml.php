53d6b6665f32438df500b0aad1b8344cc51e9e05
Frederik Glücks <frederik@gluecks-online.de>
2015-11-30 23:56:47 +0100

    Simple XML output class added.

--- libs/Output/Xml.php
+++ libs/Output/Xml.php
@@ -12,11 +12,36 @@
  **/
 namespace PhpDatabaseAnalyzer\Output;
 
-class Xml
+class Xml implements OutputInterface
 {
 
     public function __construct()
     {
         /* do something */
+    }
+
+    public function create(\PhpDatabaseAnalyzer\Logger $Logger)
+    {
+        $dom = new \DOMDocument('1.0', 'UTF-8');
+        $dom->formatOutput = true;
+
+        $root = $dom->createElement("PhpDatabaseAnalyzerLog");
+        $dom->appendChild($root);
+
+        foreach ($Logger->getLog() as $logEntry) {
+            $line = $dom->createElement("line");
+            $root->appendChild($line);
+
+            foreach ($logEntry as $fieldName => $fieldValue) {
+                if ($fieldName == 'timestamp') {
+                    $fieldValue = date("Y-m-d H:i:s", $fieldValue);
+                }
+
+                $field = $dom->createElement($fieldName, $fieldValue);
+                $line->appendChild($field);
+            }
+        }
+
+        print $dom->saveXML($root);
     }
 }

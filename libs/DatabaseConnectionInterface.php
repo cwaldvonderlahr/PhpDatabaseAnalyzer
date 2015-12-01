031428102ed35a53dc5ba3740608eca2d1e613b3
Frederik Glücks <frederik@gluecks-online.de>
2015-12-01 22:29:57 +0100

    CodeStyle errors corrected

--- libs/DatabaseConnectionInterface.php
+++ libs/DatabaseConnectionInterface.php
@@ -5,38 +5,38 @@ interface DatabaseConnectionInterface
 {
 
     public function __construct();
-	
+
     public function set($host, $username, $password, $database, $port, $charset);
-	
+
     public function getHost();
-	
+
     public function getUsername();
-	
+
     public function getPassword();
-	
+
     public function getDatabase();
-    
+
     public function getPort();
-    
+
     public function getCharset();
-    
+
     public function setHost($host);
-    
+
     public function setUsername($username);
-    
+
     public function setPassword($password);
-    
+
     public function setDatabase($database);
-    
+
     public function setPort($port);
-    
+
     public function setCharset($charset);
-    
+
     public function query($query);
-    
+
     public function getArray($query);
-    
+
     public function getRow($query);
-    
+
     public static function getInstance();
 }

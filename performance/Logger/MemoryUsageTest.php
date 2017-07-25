39b1f3f793610e1cc7e0862e0b26f8660162f4f0
Christian Wald-von der Lahr <mail@wald-vonderlahr.de>
2017-07-25 23:40:11 +0200

    Fix Unused Code Bug

--- performance/Logger/MemoryUsageTest.php
+++ performance/Logger/MemoryUsageTest.php
@@ -27,7 +27,7 @@ class MemoryUsageTest extends \PHPUnit_Framework_TestCase
     protected function tearDown()
     {
         $this->Logger = null;
-        
+
         parent::tearDown();
     }
 
@@ -39,7 +39,7 @@ class MemoryUsageTest extends \PHPUnit_Framework_TestCase
     {
         $numberOfEntries = 10000;
         $entries = array();
-        
+
         for ($i = 0; $i < $numberOfEntries; $i ++) {
             $str = md5(time());
             $str = substr($str, 0, rand(1, 20));
@@ -48,23 +48,25 @@ class MemoryUsageTest extends \PHPUnit_Framework_TestCase
                 'text' => $str
             );
         }
-        
+
         $startMemory = memory_get_usage();
-        
+
         $this->Logger = new \PhpDatabaseAnalyzer\Logger('full');
-        
+
         foreach ($entries as $entry) {
             if ($entry['type'] == 'info') {
                 $this->Logger->setInfo($entry['text']);
             }
         }
-        
+
         $log = array();
         $log = $this->Logger->getLog();
-        
+
         $endMemory = memory_get_usage();
-        
+
         echo "Memory used: " . round(($endMemory - $startMemory) / 1024 / 1024, 4) . " MB\n";
         echo "Peak: " . round((memory_get_peak_usage() - $startMemory) / 1024 / 1024, 4) . " MB\n";
+
+    	return $log;
     }
 }
